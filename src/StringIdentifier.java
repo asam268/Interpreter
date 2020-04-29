65481f0980c316288d8e7be38fce057a6bb193d4
James Bozhkov <36495009+jamesb97@users.noreply.github.com>
2020-04-28 22:15:35 -0400

    updated stringidentifier.java file

--- src/StringIdentifier.java
+++ src/StringIdentifier.java
@@ -1,32 +1,28 @@
 /**
  * StringIdentifier.java
- * Noah Huck
- * CS4308
- * Section 03
- * 22 March 2019
+ * 
  */
 
-//package pkg;
-
+//Method class Identifier implementation
 public class StringIdentifier extends Identifier {
 
     private String value;
     //class serves as template for String Identifiers in IDTable of parser
     public StringIdentifier(String name, int type, String value) {
-        super(name, type);
-        this.value = value;
+        super(name, type);  //instantiate name and type.
+        this.value = value; //set this.value to value.
     }
     @Override
     public String getValue(){
-        return value;
+        return value; //output for getValue
     }
     @Override
     public void setValue(String newValue){
-        value = newValue;
+        value = newValue;  //set value to newValue
     }
     @Override
     public String toString(){
-        return "["+name+","+type+","+value+"]";
+        return "["+name+","+type+","+value+"]"; //expected output
     }
     
 
