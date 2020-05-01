da4100db5b3e2de23abea5da20dc8de51e9e0f9a
James Bozhkov <36495009+jamesb97@users.noreply.github.com>
2020-04-30 22:41:47 -0400

    added comments for interpreter class

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -1,46 +1,50 @@
 /**
- * SCLMain.java
- * Noah Huck
- * CS4308
- * Section 03
- * 25 February 2019     Revised 25 March 2019
+ * Author: Chukwufunayan Ojiagbaje, James Bozhkov, Asa Marshall
+        Class: CS 4308 W01
+        University: Kennesaw State University
+        Professor: Dr. Jose Garrido
+        Date: April 28th, 2020
+
+
+        Title: Semester Project Deliverable 3
+        SCL Language Interpreter
  */
 
-//package pkg;
+//Interpreter class
 
 public class Interpreter {
 
     public static void main(String[] args) {
-        //file name is passed in as argument
+        //file name is developed in as an argument
                  
         if (args.length > 1){
-            switch (args[0]){
-                case "-p":
+            switch (args[0]){ //switch argument concieved
+                case "-p": //first case
                     System.out.printf("\nNow parsing file %s \n\n", args[1]);
-                    Parser p = new Parser(args[1]);
+                    Parser p = new Parser(args[1]);     //set parser to new parser
                     while (!p.endOfFileReached()){      //executes until end of file is reached
                         p.parseProgram();
                         // parseProgram parses for 1 complete function at a time
-                        // loop will execute until all functions have been parsed
+                        // loop will execute until all of the listed functions have been parsed
                     }
-                    System.out.println("\nEnd of file reached.");
+                    System.out.println("\nEnd of file reached."); //print result stating end of file reached
                     break;
-                case "-s":
+                case "-s":  //second case
                     System.out.printf("\nNow Scanning file %s \n\n", args[1]);
                     Scanner s = new Scanner(new java.io.File(args[1]));
-                    while (!s.endOfFileReached()){      //executes until end of file is reached
+                    while (!s.endOfFileReached()){      //executes until end of file has been reached
 
-                        //next token is converted to a string by convertType function
+                        //next token is converted to a string by the convertType function
                         System.out.printf("%s, row %d, col %d, symbol: %s\n",
                                 s.convertType(s.nextToken()), s.getRow(), s.getCol(), s.currentLexeme);
-                    }
-                    System.out.println("\nEnd of file reached.");
+                    } //
+                    System.out.println("\nEnd of file reached."); //print result stating end of file reached
                     break;
-                case "-e":
+                case "-e": //third case
                     System.out.printf("\nNow Executing file %s \n\n", args[1]);
-                    Parser p2 = new Parser(args[1]);
-                    p2.running = true;
-                    while (!p2.endOfFileReached()){      //executes until end of file is reached
+                    Parser p2 = new Parser(args[1]);   //set parser p2 to new parser arugument one
+                    p2.running = true; //set running parser to true 
+                    while (!p2.endOfFileReached()){      //executes until end of file has been reached
                         p2.parseProgram();
                         // parser both parses line by line and then executes corresponding function
                         // loop will execute until all functions have been parsed
@@ -49,12 +53,12 @@ public class Interpreter {
             }
         } else {
         System.out.printf("\nNow Executing file: %s \n\n", args[0]);
-            Parser p2 = new Parser(args[0]);
-            p2.running = true;
-            while (!p2.endOfFileReached()){      //executes until end of file is reached
+            Parser p2 = new Parser(args[0]); //set parser p2 to new parser with argument 0
+            p2.running = true;  //set running parser to true
+            while (!p2.endOfFileReached()){      //executes until end of file has been reached
                 p2.parseProgram();
-                // parser both parses line by line and then executes corresponding function
-                // loop will execute until all functions have been parsed
+                // parser both parses line by line and then executes the corresponding function
+                // loop will execute until all of the functions have been parsed
             }
         }
 
